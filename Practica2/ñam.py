43f052dd4d0f64a330db89f888a15655c690342c
nah2703 <juandiego27her@gmail.com>
2026-02-09 14:37:49 -0500

    Update ñam.py

--- Practica2/ñam.py
+++ Practica2/ñam.py
@@ -1,10 +1,7 @@
-n1=3
-n2=3.0
-n3="3"
-print()
-print(" n1 es:")
-print( type (n1))
-print(" n2 es:")
-print( type (n2))
-print(" n3 es:")
-print( type (n3))
+n1 = 3
+n2 = 3.0
+n3 = "3"
+
+print(f"n1 es: {type(n1)}")
+print(f"n2 es: {type(n2)}")
+print(f"n3 es: {type(n3)}")
