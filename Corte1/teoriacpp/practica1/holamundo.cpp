f9ae6a33e9db3b297eb06533c49d62996e24bf51
nah2703 <juandiego27her@gmail.com>
2026-02-11 15:17:38 -0500

    Add files via upload

---
+++ Corte1/teoriacpp/practica1/holamundo.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Biblioteca necesaria para imprimir en pantalla
+
+int main() {
+    // std::cout se usa para mostrar el mensaje
+    std::cout << "Hola Mundo" << std::endl;
+    return 0; // Indica que el programa finalizó correctamente
+}
+
