71787484e0d06f27048785448463fa7affcb60b2
nah2703 <juandiego27her@gmail.com>
2026-02-18 15:03:46 -0500

    Add files via upload

---
+++ Corte1/teoria/practica2/practica2.py
@@ -0,0 +1,8 @@
+texto = "hola mundo" 
+Entero = 22
+decimal = 12.2
+
+
+print ("hola mundo es de tipo", type (texto))
+print ("hola mundo es de tipo", type (Entero))
+print ("hola mundo es de tipo", type (decimal))
